bcb9789f1b33d07adb3c872a479f2a0236cf5b91
github username <github email address>
2018-11-21 15:36:36 -0500

    OO Basics

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  # :author =
+  # def author=(author)
+  #   @author = author
+  # end
+  attr_reader :title
+  # :title =
+  # def title
+  #   @title
+  # end
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts("Flipping the page...wow, you read fast!")
+  end
+
+end
